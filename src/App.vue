f440666bd338624aaddbb245425df96a05703bc3
ronger <ronger-x@outlook.com>
2018-12-12 16:01:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,159 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container class="wrapper">
+        <el-header>
+            <div class="vertical-container">
+                <a class="navbar-brand" href="/">
+                    <img src="./assets/logo.png" alt="Vertical" class="navbar-brand-img">
+                    <span>罗吉网</span>
+                </a>
+            </div>
+        </el-header>
+        <el-main>
+            <keep-alive>
+                <component
+                        v-bind:is="currentPageComponent"
+                        class="vertical-container"
+                        v-on:changeCurrentPage="changePage"
+                ></component>
+            </keep-alive>
+        </el-main>
+        <el-footer>
+            <Footer></Footer>
+        </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VerticalHome from "./pages/index";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {VerticalHome},
+    data () {
+        return {
+            currentPage: 'VerticalHome'
+        }
+    },
+    computed: {
+        currentPageComponent: function () {
+            return this.currentPage
+        }
+    },
+    methods: {
+        changePage(data){
+            this.currentPage = data;
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    h1 a, h2 a, h3 a, h4 a, h5 a, h6 a, .h1 a, .h2 a, .h3 a, .h4 a, .h5 a, .h6 a {
+        color: inherit;
+    }
+    h4, .h4 {
+        font-size: 1.125rem;
+    }
+
+    h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
+        margin-bottom: 0.66em;
+        font-family: inherit;
+        font-weight: 600;
+        line-height: 1.1;
+        color: inherit;
+    }
+    body {
+        margin: 0;
+        display: block;
+        background-color: #f5f7fb
+    }
+    .navbar-brand {
+        color: inherit;
+        margin-right: 1rem;
+        font-size: 1.25rem;
+        white-space: nowrap;
+        font-weight: 600;
+        padding: 0;
+        transition: .3s opacity;
+        line-height: 2rem;
+    }
+    .navbar-brand-img {
+        height: 2rem;
+        line-height: 2rem;
+        vertical-align: bottom;
+        margin-right: .5rem;
+        width: auto;
+    }
+    .el-header {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        background: #fff;
+        border-bottom: 1px solid rgba(0, 40, 100, 0.12);
+        z-index: 80;
+    }
+    .el-row {
+        margin-bottom: 20px;
+    }
+    .el-row:last-child {
+         margin-bottom: 0;
+     }
+    .header-lab {
+        padding: 30px 0;
+    }
+    .menu, .wrapper, .el-menu {
+        height: 100%;
+    }
+    .vertical-container {
+        max-width: 1200px;
+    }
+    .vertical-container {
+        width: 100%;
+        padding-right: 0.75rem;
+        padding-left: 0.75rem;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    .pt-5, .py-5 {
+        padding-top: 1.5rem !important;
+    }
+    .text-default {
+        color: #495057 !important;
+    }
+    .text-muted {
+        color: #9aa0ac !important;
+    }
+    .d-block {
+        display: block !important;
+    }
+    small, .small {
+        font-size: 87.5%;
+        font-weight: 400;
+    }
+    .avatar-md {
+        width: 2.5rem;
+        height: 2.5rem;
+        line-height: 2.5rem;
+        font-size: 1rem;
+    }
+    .avatar {
+        width: 2rem;
+        height: 2rem;
+        line-height: 2rem;
+        border-radius: 50%;
+        display: inline-block;
+        background: #ced4da no-repeat center/cover;
+        position: relative;
+        text-align: center;
+        color: #868e96;
+        font-weight: 600;
+        vertical-align: bottom;
+        font-size: .875rem;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+    .mr-3, .mx-3 {
+        margin-right: 0.75rem !important;
+    }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,60 @@
 <template>
-    $END$
+    <el-row>
+        <el-col :span="16">
+            <el-row>
+                <el-col v-for="article in articles">
+                    <el-card>
+                        <div class="card-body d-flex flex-column">
+                            <h4><a v-bind:href="article.articlePermalink">{{ article.articleTitle }}</a></h4>
+                            <div class="text-muted article-summary-md">{{ article.articleSummary }}
+                            </div>
+                            <el-row class="pt-5">
+                                <el-col :span="1" class="mr-3">
+                                    <div class="avatar avatar-md"
+                                         :style="{backgroundImage:'url(' + article.thinker.userAvatarUrl + ')'}">
+                                    </div>
+                                </el-col>
+                                <el-col :span="4">
+                                    <div>
+                                        <a v-bind:href="article.thinker.userUrl" class="text-default">{{ article.thinker.nickName }}</a>
+                                        <small class="d-block text-muted">{{ article.timeAgo }}</small>
+                                    </div>
+                                    <div class="ml-auto text-muted">
+                                        <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i
+                                                class="fe fe-heart mr-1"></i></a>
+                                    </div>
+                                </el-col>
+                            </el-row>
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </el-col>
+        <el-col :span="8">
+
+        </el-col>
+    </el-row>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "VerticalHome",
+        data() {
+            return {
+                articles: [],
+                pagination: {
+                    "paginationPage": 1,
+                    "paginationSize": 20
+                }
+            }
+        },
+        async mounted () {
+            const responseTopData = await this.axios.get('/api/getArticles')
+            if (responseTopData) {
+                this.$set(this, 'articles', responseTopData.data.data)
+                this.$set(this, 'pagination', responseTopData.data.pagination)
+            }
+        }
     }
 </script>
 
